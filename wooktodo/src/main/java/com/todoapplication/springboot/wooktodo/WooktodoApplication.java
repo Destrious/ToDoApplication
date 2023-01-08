b173552fe438444b451f9c0b5e8850fa4ad8bf2a
Marcus Archilles <marc9144@uni.sydney.edu.au>
2023-01-08 17:08:06 +1100

    Add Spring Project

---
+++ wooktodo/src/main/java/com/todoapplication/springboot/wooktodo/WooktodoApplication.java
@@ -0,0 +1,13 @@
+package com.todoapplication.springboot.wooktodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooktodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooktodoApplication.class, args);
+	}
+
+}
---
+++ wooktodo/src/test/java/com/todoapplication/springboot/wooktodo/WooktodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoapplication.springboot.wooktodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooktodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
